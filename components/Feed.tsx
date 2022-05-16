8c03a67d114e5e05e63654c0e7793ec425d7f7ec
darrylwongqz <84469361+darrylwongqz@users.noreply.github.com>
2022-05-16 20:04:14 +0800

    added feed and widget components

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,7 +1,15 @@
+import { RefreshIcon } from '@heroicons/react/outline'
 import React from 'react'
 
 function Feed() {
-  return <div>Feed</div>
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
+        <RefreshIcon className="w-8 h-8 mt-5 mr-5 transition-all duration-500 ease-out cursor-pointer text-twitter hover:rotate-180 active:scale-125" />
+      </div>
+    </div>
+  )
 }
 
 export default Feed
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
+import Widgets from '../components/Widgets'
 
 const Home: NextPage = () => {
   return (
@@ -15,6 +16,8 @@ const Home: NextPage = () => {
         <Sidebar />
 
         <Feed />
+
+        <Widgets />
       </main>
     </div>
   )
