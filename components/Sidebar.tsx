01b96bad2ce964ff44ab5a03e96ca2d8b05fae3a
darrylwongqz <84469361+darrylwongqz@users.noreply.github.com>
2022-05-16 16:43:14 +0800

    added sidebar and hover effects, initialized feed

---
+++ components/Sidebar.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {
+  BellIcon,
+  HashtagIcon,
+  BookmarkIcon,
+  CollectionIcon,
+  DotsCircleHorizontalIcon,
+  MailIcon,
+  UserIcon,
+  HomeIcon,
+} from '@heroicons/react/outline'
+import SidebarRow from './SidebarRow'
+
+const Sidebar = () => {
+  return (
+    <div className="flex flex-col">
+      <img
+        className="w-10 h-10"
+        src="/dezeen_twitter-bird.webp"
+        alt="twitter logo"
+      />
+      <SidebarRow Icon={HomeIcon} title="Home" />
+      <SidebarRow Icon={HashtagIcon} title="Explore" />
+      <SidebarRow Icon={BellIcon} title="Notifications" />
+      <SidebarRow Icon={MailIcon} title="Messages" />
+      <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
+      <SidebarRow Icon={CollectionIcon} title="Lists" />
+      <SidebarRow Icon={UserIcon} title="Sign In" />
+      <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
+    </div>
+  )
+}
+
+export default Sidebar
